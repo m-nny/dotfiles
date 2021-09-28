82cbba9b0dca4ad3e678a9a79bb47f075b8f01db
Alibek Manabayev <m-nny@yandex.ru>
2021-09-28 13:43:12 +0600

    [manjaro] small fixes

--- manjaro/01.install-oh-my-zsh.zsh
+++ manjaro/01.install-oh-my-zsh.zsh
@@ -1,10 +1,16 @@
 #!/usr/bin/env zsh
-echo shell: $SHELL
+
+echo "install oh my zsh"
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+echo "Using shell: $SHELL"
 
 ZSH_CUSTOM=$ZSH/custom
-echo $ZSH_CUSTOM
+echo "zsh custom: $ZSH_CUSTOM"
 
+echo "cloning spaceship"
 git clone https://github.com/spaceship-prompt/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1
 
 # Symlink spaceship.zsh-theme to oh-my-zsh custom themes directory
+echo "symlinking spaceship"
 ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
