d982e928ce2fdaf043b7fd28736cadf9f72cfa12
Alibek Manabayev <alibek.manabayev@gmail.com>
2024-04-23 03:26:59 +0100

    common: modulize nvim config

---
+++ dotfiles.common/config/nvim/lua/m-nny/plugins/init.lua
@@ -0,0 +1,29 @@
+return {
+	-- Detect tabstop and shiftwidth automatically
+	"tpope/vim-sleuth",
+
+	-- "gc" to comment visual regions/lines
+	{ "numToStr/Comment.nvim", opts = {} },
+
+	-- Adds git related signs to the gutter, as well as utilities for managing changes
+	{
+		"lewis6991/gitsigns.nvim",
+		opts = {
+			signs = {
+				add = { text = "+" },
+				change = { text = "~" },
+				delete = { text = "_" },
+				topdelete = { text = "‾" },
+				changedelete = { text = "~" },
+			},
+		},
+	},
+
+	-- Highlight todo, notes, etc in comments
+	{
+		"folke/todo-comments.nvim",
+		event = "VimEnter",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		opts = { signs = false },
+	},
+}
---
+++ dotfiles.common/config/nvim/lua/m-nny/plugins/ufo.lua
@@ -0,0 +1,21 @@
+return {
+	"kevinhwang91/nvim-ufo",
+	dependencies = { "kevinhwang91/promise-async" },
+	config = function()
+		-- nvim-ufo
+		vim.o.foldcolumn = "0" -- '0' is not bad
+		vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
+		vim.o.foldlevelstart = 99
+		vim.o.foldenable = true
+
+		vim.keymap.set("n", "zR", require("ufo").openAllFolds)
+		vim.keymap.set("n", "zM", require("ufo").closeAllFolds)
+
+		local ufo = require("ufo")
+		ufo.setup({
+			provider_selector = function(bufnr, filetype, buftype)
+				return { "lsp", "indent" }
+			end,
+		})
+	end,
+}
